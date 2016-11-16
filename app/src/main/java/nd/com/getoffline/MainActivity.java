1aa11e4d8b1f0cc5e3ce7dd41505cfc729c6945c
DravitLochan <dlochangupta@gmail.com>
2016-11-17 01:27:57 +0530

    added sharedpreferences and added prompt.

--- app/src/main/java/nd/com/getoffline/MainActivity.java
+++ app/src/main/java/nd/com/getoffline/MainActivity.java
@@ -1,15 +1,36 @@
+/*
+* here SharedPreferences are kinda messing up. without them(the  current state) is running properly. but as
+* soon as one uncoments the sharedpreference part, app crashes.
+* fix it!!
+*
+* */
+
 package nd.com.getoffline;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    //private EditText ed_url;
+    final Context context = this;
+    //private EditText result;
+    String url,name;
+    SharedPreferences pref =getSharedPreferences("GetOfflinePref",Context.MODE_PRIVATE);
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +39,18 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        /*ed_url =(EditText)findViewById(R.id.url);
+        ed_url.setVisibility(View.GONE);*/
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "add the link to the webpage in the following prompt!", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+
+                    givePrompt();
+                //ed_url.setVisibility(View.VISIBLE);
             }
         });
     }
@@ -49,4 +76,57 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void givePrompt()
+    {
+        LayoutInflater li = LayoutInflater.from(context);
+        View promptsView = li.inflate(R.layout.prompts, null);
+
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                context);
+
+        // set prompts.xml to alertdialog builder
+        alertDialogBuilder.setView(promptsView);
+
+        final EditText userInputU = (EditText) promptsView
+                .findViewById(R.id.UserInputURL);
+
+        final EditText userInputN=(EditText) promptsView
+                .findViewById(R.id.UserInputName);
+
+        // set dialog message
+        alertDialogBuilder
+                .setCancelable(false)
+                .setPositiveButton("ADD",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,int id) {
+                                // get user input and set it to result
+                                // edit text
+
+                                url=userInputU.getText().toString();
+                                name=userInputU.getText().toString();
+
+                                                    /*                                  **make changes here
+                                SharedPreferences.Editor editor =pref.edit();
+                                editor.putString(name,url);
+                                editor.commit();
+                                                    */
+                            }
+                        })
+                .setNegativeButton("Cancel",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,int id) {
+                                dialog.cancel();
+                            }
+                        });
+
+        // create alert dialog
+        AlertDialog alertDialog = alertDialogBuilder.create();
+
+        // show it
+        alertDialog.show();
+
+    }
+
 }
+
